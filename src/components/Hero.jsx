b3cbe8719090307814b7771a29ad43bfdde8ba63
Tolga-Han-Yilmaz <06thy40@gmail.com>
2023-02-08 09:49:47 +0300

    About.jsx was created

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,8 +15,7 @@ const Hero = () => {
       </div>
       <div>
         <p>
-        +3 years experienced  in IT and an expert frontend developer, interested in writing codes, skilled at developing complex solutions, creating responsive designs, possessing strong creative thinking skills, high energy and integrity. Ability to create algorithms effectively, and interact positively and communicate appropriately with team members. Quickly grasp new technologies and concepts to develop innovative and creative solutions to problems. Always eager to learn various technologies, tools and libraries. Especially interested in the Front end/Web Development, HTML, CSS, JS, React. Excited to learn new things and improve, lifetime student. 
-        </p>
+        I’m currently improving my React, JavaScript, Python, HTML, CSS, Bootstrap, Material UI skills.        </p>
         <p>
         Currently, I am working as a front end developer at <a href="https://www.mazakayazilim.com/">Mazaka Software.</a> 
         </p>
