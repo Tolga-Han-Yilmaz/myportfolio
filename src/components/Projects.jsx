c651c8ecc27a6729698d52bff544a0cee63e6d07
Tolga-Han-Yilmaz <06thy40@gmail.com>
2023-02-15 09:59:14 +0300

    projects.jsx created

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,60 @@
-import React from 'react'
-import "../scss/components/projects.scss"
+import React from "react";
+import "../scss/components/projects.scss";
 import { useProductContext } from "../context/ProductContextProvider";
-
+import external from "../assets/external.png";
+import github from "../assets/github.png";
 
 const Projects = () => {
   const { projectsItems } = useProductContext();
-  console.log(projectsItems)
 
   return (
-    <section id='projects' className='projects'>
-      {
-        projectsItems?.map((project)=>{
-          return(
-            <div>
-
-            <img src={project.img} alt="" />
-            <img src={project.img1} alt="" />
-            <p>{project.title}</p>
-            </div>
-          )
-        })
-      }
+    <section id="projects" className="projects">
+      <h2 className="numbered-heading">Some Things I've Built</h2>
+      <ul className="projects-box">
+        {projectsItems?.map((project) => {
+          const { id, img, title, description, repoLink, appLink, tools } =
+            project;
+          return (
+            <li className="projects-box--item d-flex" key={id}>
+              <div className="projects-box--item__content col-md-5 col-12">
+                <span className="featured-project">Featured Projects</span>
+                <h3 className="project-title">
+                  <a href={repoLink} target="_blank" rel="noreferrer">
+                    {title}
+                  </a>
+                </h3>
+                <div className="project-desription">
+                  <p>{description}</p>
+                </div>
+                {tools?.map((tool) => {
+                  return (
+                    <ul className="project-tech-list">
+                      <li>{tool.tool1}</li>
+                      <li>{tool.tool2}</li>
+                      <li>{tool.tool3}</li>
+                    </ul>
+                  );
+                })}
+                <div className="projects-links">
+                  <a href={repoLink} target="_blank" rel="noopener noreferrer">
+                    <img src={external} alt={title} />
+                  </a>
+                  <a href={appLink} target="_blank" rel="noopener noreferrer">
+                    <img src={github} alt={title} />{" "}
+                  </a>
+                </div>
+              </div>
+              <div className="projects-box--item__img col-md-7 col-12">
+                <a href={appLink} target="_blank" rel="noopener noreferrer">
+                  <img src={img} alt={title} />
+                </a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </section>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
--- src/data/data-projects.js
+++ src/data/data-projects.js
@@ -5,7 +5,7 @@ const projects = [
         title:"Social Media",
         description:"I aimed to make a blog page where everyone can share and see the posts, After logging in, the user can share as he wishes. You can delete and update your posts,You can also access current news on the right side of my web page.",
         repoLink:"https://github.com/Tolga-Han-Yilmaz/social-media",
-        applink:"https://thy-blog.vercel.app/",
+        appLink:"https://thy-blog.vercel.app/",
         tools:[
             {
                 tool1:"Redux-Toolkit"
